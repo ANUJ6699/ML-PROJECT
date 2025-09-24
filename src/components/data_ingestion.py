8236d9012e80b9e1501d27e15a2f16808f39bb6b
Anuj Chhetri <anujchhetri261@gmail.com>
2025-09-24 22:03:27 +0530

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,8 +3,12 @@ import sys
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
+
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -19,10 +23,11 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Data Ingestion started")
         try:
-            df = pd.read_csv('notebook\\data\\stud.csv')
+            data_path = os.path.join('notebook', 'data', 'stud.csv')
+            df = pd.read_csv(data_path)
             logging.info("Dataset read as dataframe")
 
-            os.makedirs(os.path.dirname(self.ingestion_config.test_data_path), exist_ok=True)
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
 
             logging.info("Train test split initiated")
@@ -35,8 +40,7 @@ class DataIngestion:
 
             return (
                 self.ingestion_config.train_data_path,
-                self.ingestion_config.test_data_path,
-                self.ingestion_config.raw_data_path
+                self.ingestion_config.test_data_path
             )
         
         except Exception as e:
@@ -45,4 +49,11 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data= obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    logging.info("Data transformation completed successfully")
+    logging.info(f"Train array shape: {train_arr.shape}, Test array shape: {test_arr.shape}")
